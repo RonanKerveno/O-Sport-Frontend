ec2b8b1f672f3d3a1f0fb4d73ebb824986e1cc6b
Meddy Chalder <dymehproduction@MacBook-Pro-de-Meddy.local>
2023-05-19 09:33:56 +0200

    Update

--- osport/src/components/description.tsx
+++ osport/src/components/description.tsx
@@ -1,16 +1,14 @@
 export default function Description() {
   return (
-    <div className="flex justify-center w-full">
-      <div className="overflow-auto bg-[#e5e6f6] font-sans border-solid border border-[#140f07] w-1/2 p-1 h-32 rounded-md " id="description">
-        div Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    <div className="">
+      <div className="bg-[#fff369] font-sans border-solid border-2 border-[#ffd171] p-1 w-72 h-32">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled it to make a type specimen book.
         It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged.
-        It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages,
-        and more recently with desktop publishing software
-        like Aldus PageMaker including versions of Lorem Ipsum.
+        remaining essentially unchanged. It was popularised in the 1960s with the release of
+        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </div>
     </div>
   );
--- osport/src/components/navbar.tsx
+++ osport/src/components/navbar.tsx
@@ -1,24 +1,25 @@
 import { HiHome, HiUser } from 'react-icons/hi2';
 import { HiSearch, HiPlus } from 'react-icons/hi';
-import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-16">
-      <div className="grid h-full max-w-lg grid-cols-4 mx-auto border-t-2 border-[#b430a6]">
-        <button type="button" className="bg-white inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group ">
-          <Link href="/recherche"><HiSearch size={24} color="#b430a6" /><span className="sr-only">Search</span></Link>
+
+    <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+      <div className="grid h-full max-w-lg grid-cols-4 mx-auto">
+        <button data-tooltip-target="tooltip-home" type="button" className=" border-r border-[#3293ff] bg-[#dbedf6] inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group ">
+          <HiSearch size={24} color="#0343f4" />
+          <span className="sr-only">Search</span>
         </button>
-        <button type="button" className="bg-white inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-          <Link href="/"><HiHome size={24} color="#b430a6" /></Link>
+        <button data-tooltip-target="tooltip-home" type="button" className="border-r border-[#3293ff] bg-[#dbedf6] inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+          <HiHome size={24} color="#0343f4" />
           <span className="sr-only">Home</span>
         </button>
-        <button type="button" className="bg-white inline-flex flex-col items-center justify-center px-5  hover:bg-gray-50 dark:hover:bg-gray-800 group">
-          <Link href="/connexion"><HiUser size={24} color="#b430a6" /></Link>
+        <button data-tooltip-target="tooltip-home" type="button" className="border-r border-[#3293ff] bg-[#dbedf6] inline-flex flex-col items-center justify-center px-5  hover:bg-gray-50 dark:hover:bg-gray-800 group">
+          <HiUser size={24} color="#0343f4" />
           <span className="sr-only">Profil</span>
         </button>
-        <button type="button" className="bg-white inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-          <Link href="/ajouter"><HiPlus size={24} color="#b430a6" /></Link>
+        <button data-tooltip-target="tooltip-home" type="button" className="bg-[#dbedf6] inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
+          <HiPlus size={24} color="#0343f4" />
           <span className="sr-only">Add</span>
         </button>
 
