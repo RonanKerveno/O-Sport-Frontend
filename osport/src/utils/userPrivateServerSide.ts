6d41d7136853cdbd5f5a2dbd881397e59fb3c6fd
RonanKerveno <ronan.kerveno@gmail.com>
2023-05-22 16:15:41 +0200

    Client side rendering for private data request

--- osport/src/utils/userPrivateServerSide.ts
+++ osport/src/utils/userPrivateServerSide.ts
@@ -2,13 +2,12 @@
 // de l'API avant de rendre la page.
 
 import { GetServerSidePropsContext } from 'next';
-import { UserPublicData, UserPrivateData } from '../types';
-import { getUserById, getUserByIdPrivate } from '../services/userService';
+import { UserPublicData } from '../types';
+import { getUserById } from '../services/userService';
 
 // Typage des données reçues par l'API
 interface UserServerSideProps {
   userData: UserPublicData;
-  userPrivateData: UserPrivateData;
 }
 
 const getPrivateServerSideProps = async (context: GetServerSidePropsContext):
@@ -20,29 +19,16 @@ const getPrivateServerSideProps = async (context: GetServerSidePropsContext):
     throw new Error('User ID is invalid');
   }
 
-  // Récupération du cookie dans l'objet 'req' de 'context' (objet fournit par Next.js).
-  // Comme on est en SSR on est obligé de récupérer le cookie via le contexte de Next pour le
-  // transmettre manuelement dans la reqûete
-  const { cookie } = context.req.headers;
-
-  // On lance les requêtes API.
+  // On lance la requête API.
   const userResponse = await getUserById(userId);
-  // Passer le cookie à getUserByIdPrivate
-  const userPrivateResponse = await getUserByIdPrivate(userId, cookie);
 
   // Si l'ID n'est pas trouvé on redirige vers la page 404
   if (!userResponse.success) {
     throw new Error('User not found');
   }
 
-  // Si l'ID n'est pas trouvé on redirige vers la page 404
-  if (!userPrivateResponse.success) {
-    throw new Error('Private user data not found');
-  }
-
   return {
     userData: userResponse.user,
-    userPrivateData: userPrivateResponse.userPrivate,
   };
 };
 
