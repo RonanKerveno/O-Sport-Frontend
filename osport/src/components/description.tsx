1c0ac6c19f55e3239d17071ab1ede2d53668cf18
Meddy Chalder <dymehproduction@MacBook-Pro-de-Meddy.local>
2023-05-25 09:24:29 +0200

    update

--- osport/src/components/description.tsx
+++ osport/src/components/description.tsx
@@ -1,7 +1,7 @@
 export default function Description() {
   return (
-    <div className=" overflow-auto bg-white font-sans p-1 h-32 rounded-md m-2 text-gray-600 shadow-md md:mb-5" id="description">
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    <div className="overflow-auto bg-[#e5e6f6] font-sans border-solid border border-[#140f07] w-screen p-1 h-32 md:w-auto rounded-md " id="description">
+      div Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
       It has survived not only five centuries, but also the leap into electronic typesetting,
